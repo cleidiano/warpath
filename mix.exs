32a1681af1ce7026191846962205fcd9ed9d2024
Cleidiano Oliveira <olieiracleidiano@gmail.com>
2020-04-21 10:55:26 -0300

    Add dev to version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Warpath.MixProject do
   use Mix.Project
   @description "A implementation of Jsonpath expression for Elixir."
-  @version "0.2.1"
+  @version "0.3.0-dev"
 
   def project do
     [
