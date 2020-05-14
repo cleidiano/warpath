cabbc069b834d55b2e405f563b8b778739571bbc
Cleidiano Oliveira <olieiracleidiano@gmail.com>
2020-05-14 13:47:58 -0300

    reconfigure StreamData

--- config/test.exs
+++ config/test.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :stream_data,
   max_runs:
     if(System.get_env("CI"),
-      do: IO.inspect(200, label: "CI enabled max_runs:", syntax_colors: [number: :yellow]),
+      do: IO.inspect(100, label: "CI enabled max_runs:", syntax_colors: [number: :yellow]),
       else: 10
     )
