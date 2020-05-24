eccc8f3c062afb6ff45f1dcda636ab9af6dc2b0e
Cleidiano Oliveira <Cleidiano@users.noreply.github.com>
2020-05-24 16:51:20 -0300

    Remove Task.async_stream (#60)

--- mix.exs
+++ mix.exs
@@ -32,7 +32,8 @@ defmodule Warpath.MixProject do
       {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:yaml_elixir, "~> 2.4", only: :test},
-      {:stream_data, "~> 0.1", only: [:test, :dev]}
+      {:stream_data, "~> 0.1", only: [:test, :dev]},
+      {:benchee, "~> 1.0", only: :dev}
     ]
   end
 
