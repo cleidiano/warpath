a3591c1d56afebc740adb84805bc9a4a7dfd1b7b
Cleidiano Oliveira <Cleidiano@users.noreply.github.com>
2020-06-27 09:06:49 -0300

    Handle json decode error (#78)

--- lib/warpath/exceptions.ex
+++ lib/warpath/exceptions.ex
@@ -3,3 +3,17 @@ defmodule Warpath.ExpressionError do
 
   @type t :: %__MODULE__{}
 end
+
+defmodule Warpath.JsonDecodeError do
+  defexception [:position, :data]
+
+  @type t :: %__MODULE__{position: integer, data: String.t()}
+
+  @spec from(Jason.DecodeError.t()) :: Warpath.JsonDecodeError.t()
+  def from(%Jason.DecodeError{data: data, position: position}) do
+    %Warpath.JsonDecodeError{data: data, position: position}
+  end
+
+  @impl true
+  defdelegate message(exp), to: Jason.DecodeError
+end
