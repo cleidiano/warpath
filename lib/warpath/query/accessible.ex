829e48501eb2079ee99dc806dd0eabbee68f7c1c
mmmries <riesmmm@gmail.com>
2020-10-01 20:53:08 -0600

    allow property access on filters with no matches

--- lib/warpath/query/accessible.ex
+++ lib/warpath/query/accessible.ex
@@ -7,7 +7,7 @@ defmodule Warpath.Query.Accessible do
 
   def has_key?(%{} = accessible, key), do: Map.has_key?(accessible, key)
 
-  def has_key?(keywords, key) when is_list(keywords),
+  def has_key?(keywords, key) when is_list(keywords) and is_atom(key),
     do: Keyword.keyword?(keywords) and Keyword.has_key?(keywords, key)
 
   def has_key?(_, _), do: false
